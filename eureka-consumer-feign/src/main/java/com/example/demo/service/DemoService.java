09a5a6f79402dfc1d887f4b96ed0c02122ea6d64
EasyArch-Sun <3268543958@qq.com>
2020-11-30 23:18:44 +0800

    spring-cloud-eureka

---
+++ eureka-consumer-feign/src/main/java/com/example/demo/service/DemoService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(value ="eureka-provider")
+public interface DemoService {
+
+    @GetMapping("/demo")
+    String consumer();
+}
+
