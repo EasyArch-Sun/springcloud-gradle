fe18a82c29021aeb64925ec2ad8833e8f441d8f1
EasyArch-Sun <3268543958@qq.com>
2020-12-03 20:28:09 +0800

    spring-cloud-consul

--- consul-provider/src/main/java/com/example/demo/DemoApplication.java
+++ consul-provider/src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,11 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
-@EnableConfigurationProperties(DemoConfig.class)
+//@EnableConfigurationProperties(DemoConfig.class)
 @SpringCloudApplication
 public class DemoApplication {
 
